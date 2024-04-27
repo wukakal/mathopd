4fe9239663e206a3bd0351a3163afe43978a8ae5
Michiel Boland <michiel@boland.org>
2024-04-27 13:00:41 +0200

    Bump version number, adjust CHANGES

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@
 #include <time.h>
 #include "mathopd.h"
 
-const char server_version[] = "Mathopd/1.6b15";
+const char server_version[] = "Mathopd/1.6b16";
 
 volatile sig_atomic_t gotsigterm;
 volatile sig_atomic_t gotsighup;
